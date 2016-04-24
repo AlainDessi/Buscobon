ff76b80a5efc715d083a635b8f71180a3f6c07ff
Alain Dessi <alain.dessi@laposte.net>
2016-04-24 17:39:45 +0200

    correction Authentification et modification version de AdWeb

--- app/Controller/Auth/AuthController.php
+++ app/Controller/Auth/AuthController.php
@@ -10,49 +10,72 @@ use Auth;
 use Hash;
 
 
-class AuthController extends \Core\Http\Controller {
+class AuthController extends \Core\Http\Controller
+{
+    /**
+     * login
+     * @return view
+     */
+  	public function getLogin()
+    {
+        return View('admin.auth.login');
+  	}
 
-  /**
-   * login
-   */
-	public function getLogin()
-  {
-    return View('admin.auth.login');
-	}
+    /**
+     * Logout
+     * @return redirection vers page d'accueil du site Front
+     * @todo attention erreur si la route 'homepage n'existe pas'
+     */
+    public function getLogout()
+    {
+        Auth::logout();
+        return redirect('homepage');
+    }
 
-  /**
-   * Logout
-   */
-  public function getLogout()
-  {
-    Auth::logout();
-    return redirect('homepage');
-  }
+    /**
+     * Vérification des paramètres posté
+     */
+    public function checkLogin()
+    {
+        // récupération des données de _POST
+        $post = Request::post();
 
-  public function checkLogin()
-  {
-      $post = Request::post();
+        // Lecture de l'utilisateur
+        $user = Users::where('email', '=', $post['email'])
+                ->first();
 
-      $user = Users::where('email','=',$post['email'])->first();
-
-      if($user) {
-        if(Hash::Check($post['password'], $user->password)) {
-          $this->logUser($user);
-          exit();
+        // si l'utilisateur existe et mot de passe correct
+        if($user) {
+          // Vérification du mot de passe
+          if(Hash::Check($post['password'], $user->password)) {
+              $this->logUser($user);
+              exit();
+          } else {
+              // mot de passe incorrect
+              Session::danger('Mot de passe ou Email incorrect');
+              return redirect('login');
+          }
+        } else {
+            // email incorrect
+            Session::danger('Mot de passe ou Email incorrect');
+            return redirect('login');
         }
-      }
+    }
 
-      Session::danger('Mot de passe ou Email incorrect');
-      return redirect('login');
-  }
+    /**
+     * mise en session des coordonnées et paramètres de l'utilisateur
+     *
+     * @param model $user
+     * @return redirect
+     */
+    public function logUser($user)
+    {
+        // enregistrement des données utilisateur dans Session
+        Auth::setId($user->id)
+              ->setEmail($user->email)
+              ->setRight($user->rightvalue);
 
-  public function logUser($user)
-  {
-      // enregistrement Session
-      Auth::setId($user->id)->setEmail($user->email)->setRight($user->rightvalue);
-
-      // redirect here
-      return redirect('admin.homepage');
-  }
-
-} // end class
+        // Redirection vers la page d'accueil du site
+        return redirect('admin.homepage');
+    }
+}
